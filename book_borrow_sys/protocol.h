242402a22fd60cd1e476e98c1ef7ce101611717d
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-08-07 21:55:46 +0800

    自定义网络通信协议头文件

---
+++ book_borrow_sys/protocol.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+	> File Name: protocol.h
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年08月01日 星期二 10时30分09秒
+ ************************************************************************/
+
+#ifndef _PROTOCOL_H
+#define _PROTOCOL_H
+
+#define NET_PACKET_DATA_SIZE 5000
+
+/// 网络数据包包头
+struct NetPacketHeader
+{
+    unsigned short      wDataSize;  ///< 数据包大小，包含封包头和封包数据大小
+    unsigned short      wOpcode;    ///< 操作码
+};
+
+/// 网络数据包
+struct NetPacket
+{
+    NetPacketHeader     Header;                         ///< 包头
+    char       Data[NET_PACKET_DATA_SIZE];     ///< 数据
+};
+
+
+
+/// 网络操作码
+enum eNetOpcode
+{
+    LOGIN  = 1,  //登录
+    REGISTER,  //注册
+    LOGIN_YES,  //登录成功
+    LOGIN_NO,    //登录失败
+    REGISTER_YES,  //注册成功
+    REGISTER_NO,  //注册失败
+    PERSONAL_DATA  //查询个人信息
+};
+
+
+
+void my_err(const char *err_string,int line)  //自定义错误函数
+{
+    std::cerr<<"line:"<<line<<std::endl; //输出错误发生在第几行
+    perror(err_string);       //输出错误信息提示
+    exit(1);
+}
+
+#endif
